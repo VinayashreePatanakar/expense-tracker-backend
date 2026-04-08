8103a272f69d889e0940312f2dc351fb0cf1c197
VinayashreePatanakar <vinayapatanakar@gmail.com>
2026-04-08 15:55:10 +0200

    Updates

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "expense-tracker-vinaya.netlify.app",
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Routes
